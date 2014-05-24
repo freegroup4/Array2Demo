81ba82d904dabe7bcd9e6779b7d99cb4ad7e5a15
freegroup4 <freegroup4@yopmail.com>
2014-05-25 00:01:51 +0800

    Final Commit
    Final

--- src/array2demo/Array2Demo.java
+++ src/array2demo/Array2Demo.java
@@ -5,8 +5,15 @@ public class Array2Demo {
         int i,j,sum = 0;
         for (int k = 0; k < sale.length; k++) {
             System.out.println("業務員 : " + (k+1) + "的業績為:");
+            for (int l = 0; l < sale[k].length; l++) {
+                System.out.println(sale[k][l] + " ");
+                sum += sale[k][l];
+            }
             System.out.println();
         }
         System.out.println("總業績為 : " + sum);
     }
 }
+
+
+
